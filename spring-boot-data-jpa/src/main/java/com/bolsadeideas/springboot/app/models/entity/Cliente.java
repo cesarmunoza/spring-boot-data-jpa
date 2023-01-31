08fd8ab03bffd6a75c5cbd5623984af15c0fd86e
cesar <cesare.munoz@gmail.com>
2023-01-31 17:19:44 -0500

    Se crea la clase de entidad Cliente :q

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Cliente.java
@@ -0,0 +1,36 @@
+package com.bolsadeideas.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nombre;
+	private String apellido;
+	private String email;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAte;
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
