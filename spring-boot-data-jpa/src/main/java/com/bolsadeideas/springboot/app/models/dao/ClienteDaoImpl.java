bdf0548eca86438eb93464b1ab9abbd830ef6978
cesar <cesare.munoz@gmail.com>
2023-02-01 09:47:30 -0500

    Se crea la persistencia, la interfaz y la implementación para Cliente

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,27 @@
+package com.bolsadeideas.springboot.app.models.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bolsadeideas.springboot.app.models.entity.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	@Transactional(readOnly=true)
+	@Override
+	public List<Cliente> findAll() {		
+		return em.createQuery("from Cliente").getResultList();
+		
+	}
+
+}
